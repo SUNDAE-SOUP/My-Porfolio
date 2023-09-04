1f3cf9e1d54c8b2dbea6f03b7afab44fd8181072
Rhejoel Joson <rhejoelstrong@gmail.com>
2023-09-05 05:23:20 +0800

    1st commit

---
+++ main.js
@@ -0,0 +1,40 @@
+function openNav() 
+{
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() 
+{
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+function openNav2() 
+{
+    document.getElementById("myNav2").style.width = "100%";
+}
+
+function closeNav2() 
+{
+    document.getElementById("myNav2").style.width = "0%";
+}
+
+function openNav3() 
+{
+    document.getElementById("myNav3").style.width = "100%";
+}
+
+function closeNav3() 
+{
+    document.getElementById("myNav3").style.width = "0%";
+}
+
+function openNav4() 
+{
+    document.getElementById("myNav4").style.width = "100%";
+}
+
+function closeNav4() 
+{
+    document.getElementById("myNav4").style.width = "0%";
+}
